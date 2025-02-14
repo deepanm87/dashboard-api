f6e3af533819543825fbf4cb5464bfcef12508ea
deepanm87 <deepan.murugan29@gmail.com>
2025-02-14 16:06:11 -0500

    added placeholders
    crypto time weather placeholders and flexbox alignment

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
+const userName = document.getElementById("user-name")
+
 fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature")
     .then(res => res.json())
     .then(data => 
         {
+            console.log(data)
             document.body.style.backgroundImage = `url(${data.urls.full})`
+            userName.textContent = `By: ${data.user.name}`
         })
