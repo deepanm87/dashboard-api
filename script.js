7784ef735f05fb158c49abf2840dc1bd77f1015d
deepanm87 <deepan.murugan29@gmail.com>
2025-02-17 11:41:57 -0500

    add failsafe image

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 const userName = document.getElementById("user-name")
 
-fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature")
+fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=sdsdsds")
     .then(res => res.json())
     .then(data => 
         {
-            console.log(data)
             document.body.style.backgroundImage = `url(${data.urls.full})`
             userName.textContent = `By: ${data.user.name}`
         })
+    .catch(err => {
+        document.body.style.backgroundImage = "url(https://images.unsplash.com/photo-1539678786826-79e8b51b457a?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3wxNDI0NzB8MHwxfHJhbmRvbXx8fHx8fHx8fDE3Mzk3NDg2MDF8&ixlib=rb-4.0.3&q=80&w=1080)"
+    })
+
+
+fetch("")
